9d81727afba8027069f7a225f2e7f29529b81fb5
rayannecavalcante <you@example.com>
2021-10-20 10:40:40 -0300

    bendito seja deus

---
+++ Code/js/jscode.js
@@ -0,0 +1,47 @@
+const botao = document.getElementById("bt")
+const secao2 = document.getElementById("s2")
+const imgMao = document.getElementById("imgsecao2")
+const texts2 = document.getElementById("psecao2")
+secao2.style.height="800px"
+
+function removetext(){
+    texts2.innerHTML="A energia solar deve ser utilizada pois contribui para a preservação do meio ambiente, é uma fonte de energia limpa e renovável, auxiliando na economia de energia elétrica. Além disso ela conta com uma instalação fácil, valoriza seu imóvel e possibilita a geração de créditos energéticos."
+}
+
+function addimg (){
+
+    const img = document.createElement("img")
+    img.src = "js/plantas.png"
+    img.style.width="400px"
+    img.style.float="right"
+    img.style.marginTop="140px"
+    img.style.paddingRight="50px"
+    
+    imgMao.parentNode.replaceChild(img,imgMao)
+
+}
+function removeImage(){
+    imgMao.remove()
+}
+ function changeColor (){
+      
+    secao2.classList.remove("section2")
+    secao2.classList.add("novacor")
+ 
+ 
+} function changeMode(){
+    addimg();
+    changeColor();
+    removeImage();
+    removetext();
+
+}
+
+function npega (){
+  
+    
+    secao2.classList.toggle("novacor")
+ 
+ }
+
+botao.addEventListener('click',changeMode)
